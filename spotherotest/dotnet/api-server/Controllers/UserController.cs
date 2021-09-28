38033e98d1f89be98d347ca9eb2301ada1d7ab7f
Peter McGuire <petermcgu@gmail.com>
2021-09-28 11:12:41 -0700

    very basic functionality, no error checking

--- spotherotest/dotnet/api-server/Controllers/UserController.cs
+++ spotherotest/dotnet/api-server/Controllers/UserController.cs
@@ -1,8 +1,9 @@
 ﻿using System.Collections.Generic;
+using System;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using api_server.Models;
-
+using Microsoft.EntityFrameworkCore;
 
 namespace api_server.Controllers
 {
@@ -17,8 +18,30 @@ namespace api_server.Controllers
         }
 
         [HttpGet]
-        public ActionResult GetUsers() {
-            return Ok(_context.Users.ToList());
+        public ActionResult GetUsers([FromQuery]string active) {
+            List<User> result = new List<User>();
+            if (active is null) {
+                result = _context.Users.ToList();
+            } else {
+                result = _context.Users.Where(u => u.Active == (active == "true" ? true : false)).ToList();
+            }
+
+            return Ok(result);
+        }
+        [HttpGet("{Id}")]
+        public ActionResult GetUser(int Id) {
+            return Ok(_context.Users.Where(u => u.Id == Id));
+        }
+        [HttpGet("{Id}/worked_hours")]
+        public ActionResult GetUserWorkedHours(int Id) {
+            return Ok(_context.WorkedHours.Where(wh => wh.UserId == Id).ToList());
+        }
+        [HttpPost("{Id}/worked_hours")]
+        public ActionResult<WorkedHour> PostUserWorkedHours(int Id, WorkedHour wh) {
+            wh.UserId = Id;
+            _context.Add(wh);
+            _context.SaveChanges();
+            return Ok(wh);
         }
     }
 }
